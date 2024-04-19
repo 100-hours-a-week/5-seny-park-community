f5c4c22197408c3e003d1b8d6a14382b81b25a58
sen2y <pse6704@sookmyung.ac.kr>
2024-04-20 00:53:29 +0900

    feature/ js event 적용 완료 ( + 댓글 수정시 textarea에 내용반영, 버튼 수정버튼으로 변경)

--- public/js/post.js
+++ public/js/post.js
@@ -10,12 +10,15 @@ const bodyEl = document.querySelector("body");
 const modalPostEl = document.querySelector(".shadow-post");
 const modalCommentEl = document.querySelector(".shadow-comment");
 // 댓글 창
-const commentEl = document.querySelector(".write-comment");
-const commentBtn = document.querySelector(".inner .makeComment .btn button");
+const commentEl = document.querySelector(".write-comment"); // form textarea
+const commentBtn = document.querySelector(".inner .makeComment .btn button"); // 댓글 등록 버튼
 const modiCoBtn = document.querySelector(".comments .modi");
 const delCoBtn = document.querySelector(".comments .del");
 const cancelCoBtn = document.querySelector(".shadow-comment .cancel");
 const confirmCoBtn = document.querySelector(".shadow-comment .delete");
+const formEl = document.querySelector("form");
+
+const commentEditEl = document.querySelector(".comment.active");
 
 // 게시글 및 댓글 모달 이벤트 리스너 설정
 setupModalToggle(delBtn, modalPostEl, bodyEl);
@@ -31,5 +34,23 @@ commentEl.addEventListener("input", () => {
     commentBtn.classList.add("active");
   } else {
     commentBtn.classList.remove("active");
+  }
+});
+
+// 댓글 수정 버튼 클릭 시 댓글 입력창에 댓글 내용 추가되고 수정 버튼 value값은 댓글 수정으로 바뀐다.
+// 클릭된 commentsEl 요소 안에 댓글 내용 요소인 div 수정버튼(.modi) 클릭 시 .comments의 댓글내용인 .comments .comment의 내용을 가져와서 commentEl에 넣어준다.
+modiCoBtn.addEventListener("click", () => {
+  console.log(commentEditEl.textContent);
+  commentEl.value = commentEditEl.textContent;
+  commentBtn.textContent = "댓글 수정";
+  commentBtn.classList.add("modi");
+  commentEl.focus();
+});
+
+// 댓글 수정 버튼 클릭 시 댓글 수정 버튼이 댓글 등록 버튼으로 바뀐다.
+commentBtn.addEventListener("click", () => {
+  if (commentBtn.classList.contains("modi")) {
+    commentBtn.textContent = "댓글 등록";
+    commentBtn.classList.remove("modi");
   }
 });
