521a56619c86b52f0b6a25f420534fcb1d01d8f3
sen2y <pse6704@sookmyung.ac.kr>
2024-06-16 02:30:13 +0900

    feature/ 게시글 목록 get - mysql 연동 완료 및 이미지 주소 잘 안가져오는 문제 해결 및 main.js 코드 리팩토링

--- fe-server/public/js/main.js
+++ fe-server/public/js/main.js
@@ -1,65 +1,51 @@
 import { formatDate, changeNum } from "/js/utils.js";
 const postsContainer = document.querySelector(".posts");
 
-// fetch로 json 파일 불러오기
-const posts = [];
 // 얘는 미리받아와도 된다
+// fetch로 json 파일 불러오기
 fetch("http://localhost:4000/posts", {
   credentials: "include", // 쿠키를 요청과 함께 보내도록 설정
 })
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    data.forEach((post) => {
-      posts.push({
-        post_id: post.post_id,
-        title: post.post_title,
-        content: post.post_content,
-        like: post.like,
-        comment_cnt: post.comments ? post.comments.length : 0,
-        hit: post.hits,
-        date: formatDate(post.created_at),
-        author: post.nickname,
-        profile: post.profileImagePath,
-      });
-    });
-    console.log(posts.length);
-    render();
+    render(data);
   });
 
-const render = () => {
-  if (posts.length) {
+const render = (data) => {
+  if (data.length) {
     console.log(1);
-    postsContainer.innerHTML = ""; // 게시글 초기화
-    posts.forEach((post) => {
-      // 게시글 제목 26글자 이내
-      const postDiv = document.createElement("div");
-      postDiv.className = "post";
-      const url = `/main/post?post_id=${post.post_id}`;
-      postDiv.innerHTML = `
-    <a href="${url}">
-      <div class="content one">
-      <h2>${post.title.substring(0, 26)}</h2>
-        <div class="texts">
-          <div class="left">
-            <p>좋아요 ${changeNum(post.like)}</p>
-            <p>댓글 ${changeNum(post.comment_cnt)}</p>
-            <p>조회수 ${changeNum(post.hit)}</p>
+    const postElements = data
+      .map((post) => {
+        const url = `/main/post?post_id=${post.post_id}`;
+
+        return `
+          <div class="post">
+            <a href="${url}">
+              <div class="content one">
+                <h2>${post.post_title.substring(0, 26)}</h2>
+                <div class="texts">
+                  <div class="left">
+                    <p>좋아요 ${changeNum(post.likes)}</p>
+                    <p>댓글 ${changeNum(post.comments)}</p>
+                    <p>조회수 ${changeNum(post.hits)}</p>
+                  </div>
+                  <div class="right">
+                    <p>${formatDate(post.created_at)}</p>
+                  </div>
+                </div>
+              </div>
+              <div class="writer">
+                <div class="img" style="background-image: url('${
+                  post.profile_image.replace("/images", "") // '/images' 제거
+                }');"></div>
+                <div class="name">${post.nickname}</div>
+              </div>
+            </a>
           </div>
-          <div class="right">
-            <p>${post.date}</p>
-          </div>
-        </div>
-      </div>
-      <div class="writer">
-        <div class="img" style="background-image: url('${
-          post.profile
-        }');"></div>
-        <div class="name">${post.author}</div>
-      </div>
-  </a>
-    `;
-      postsContainer.appendChild(postDiv);
-    });
+        `;
+      })
+      .join("");
+    postsContainer.innerHTML = postElements;
   }
 };
